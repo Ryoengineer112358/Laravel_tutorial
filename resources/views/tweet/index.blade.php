de769a0b25c15c6184cb5af6390c585e4f9de403
Ryoengineer112358 <ryoengineer112358@gmail.com>
2022-06-29 17:53:47 +0900

    変ん週内容の更新処理

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -9,21 +9,28 @@
 </head>
 <body>
 <h1>つぶやきアプリ</h1>
-    <div>
-        <p>投稿フォーム</p>
-        <form action="{{ route('tweet.create') }}" method="post">
-            @csrf
-            <label for="tweet-content">つぶやき</label>
-            <span>140文字まで</span>
-            <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
-            @error('tweet')
-            <p style="color: red;">{{ $message }}</p>
-            @enderror
-            <button type="submit">投稿</button>
-        </form>
-        @foreach($tweets as $tweet)
-            <p>{{ $tweet->content }}</p>
-        @endforeach
-    </div>
+<div>
+    <p>投稿フォーム</p>
+    <form action="{{ route('tweet.create') }}" method="post">
+        @csrf
+        <label for="tweet-content">つぶやき</label>
+        <span>140文字まで</span>
+        <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
+        @error('tweet')
+        <p style="color: red;">{{ $message }}</p>
+        @enderror
+        <button type="submit">投稿</button>
+    </form>
+</div>
+<div>
+    @foreach($tweets as $tweet)
+        <details>
+            <summary>{{ $tweet->content }}</summary>
+            <div>
+                <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+            </div>
+        </details>
+    @endforeach
+</div>
 </body>
 </html>
